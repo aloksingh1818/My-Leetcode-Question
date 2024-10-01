0b8f5834eec48482b72bf6ff10fac41da4d0ea88
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-01 18:15:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (76.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original=x;
+        int revNumber=0;
+        while(x>0){
+            int digit=x%10;
+            if(revNumber > (INT_MAX - digit)/10){
+            return false;
+            }
+            revNumber=revNumber*10+digit;
+            x=x/10;
+            
+        }
+
+
+
+        if(revNumber==original){
+            return true;
+        }
+       
+        else{
+            return false;
+        }
+    }
+};
