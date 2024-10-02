9038dd557658933ff06627ae2fc6a8dd48abc142
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-02 14:41:24 +0530

    Time: 0 ms (100%), Space: 7.6 MB (26.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-          if(n==1){
-            return true;
-          }
-          if(n<=0 || n%2 !=0){
+        if(n==0){
             return false;
-          }
-          return isPowerOfTwo(n/2);
+        }
+        if(floor(log2(n))== ceil(log2(n))){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+          //if(n==1){
+           // return true;
+         // }
+         // if(n<=0 || n%2 !=0){
+           // return false;
+         // }
+         // return isPowerOfTwo(n/2);
     }
 };
