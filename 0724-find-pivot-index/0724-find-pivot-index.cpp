e5acaee7f35cadb6d59dcae8043435e29cd308e7
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-03 22:25:39 +0530

    Time: 15 ms (73.32%), Space: 35.9 MB (15.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,27 +1,48 @@
+//            -----------------OPTIMAL SOLUTION --------------------
+
 class Solution {
 public:
-    int bruteforce(vector<int>& nums) {
+    int pivotIndex(vector<int>& nums) {
+        vector<int>lsum(nums.size(),0);
+        vector<int>rsum(nums.size(),0);
+        for(int i=1;i<nums.size();i++){
+            lsum[i]=lsum[i-1]+nums[i-1];
+        }
+        for(int i=nums.size()-2;i>=0;i--){
+            rsum[i]=rsum[i+1]+nums[i+1];
+        }
         for(int i=0;i<nums.size();i++){
-            int lsum=0;
-            int rsum=0;
+            if(lsum[i]==rsum[i]) return i;
+        }
+        return -1;
         
-            for(int j=0;j<i;j++){
-                 lsum=lsum+nums[j];
-                    }
-             for(int j=i+1;j<nums.size();j++){
-                    rsum=rsum+nums[j];
-                        }
-                if(lsum==rsum) return i;
         
-
-            }    
-         return -1;
-        } 
-        
-    
-    
-        int pivotIndex(vector<int>& nums) {
-        
-      return bruteforce(nums);  
     }
 };
+
+//            --------------BRUTE FORCE--------------------
+/*
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            int lsum = 0, rsum = 0;
+
+            for(int j = 0; j < i; j++) {
+                lsum += nums[j];
+            }
+
+            
+            for(int j = i + 1; j < nums.size(); j++) {
+                rsum += nums[j];
+            }
+
+    
+            if(lsum == rsum) return i;
+        }
+
+        return -1;
+        
+    }
+};
+*/
