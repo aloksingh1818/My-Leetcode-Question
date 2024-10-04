62ffc70203dc7595e9955fcf97b1a4a909717b73
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-04 16:19:23 +0530

    Time: 0 ms (100%), Space: 7.4 MB (84.42%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t1=0,t2=1,t3=1,nextterm=0;
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        for(int i=3;i<=n;i++){
+            nextterm=t1+t2+t3;
+            t1=t2;
+            t2=t3;
+            t3=nextterm;
+        }
+        return t3;
+        
+        return(tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3));
+    }
+};
