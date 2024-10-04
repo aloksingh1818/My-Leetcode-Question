c5a70bd085de849dac01df0f70a606eafd2209c5
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-04 21:24:17 +0530

    Time: 5 ms (25.08%), Space: 10.2 MB (88.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,3 +1,5 @@
+//-------------COUNTING APPROACH---------------------------------
+/*
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -31,3 +33,33 @@ public:
 
     }
 };
+*/
+
+//-----------------------SORTING APPROACH------------------------------
+//sort(nums.begin(),nums.end());
+
+
+//-----------------------3POINTER OR INPLACE APPROACH----------------------------------------
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low =0;
+        int medium=0;
+        int high=nums.size()-1;
+        while(medium<=high){
+            if(nums[medium]==0){
+                swap(nums[low],nums[medium]);
+                low++,medium++;
+            }
+            else if(nums[medium]==1){
+                medium++;
+            }
+            else{
+                swap(nums[medium],nums[high]);
+                high--;
+            }
+        }
+  
+    } 
+};  
