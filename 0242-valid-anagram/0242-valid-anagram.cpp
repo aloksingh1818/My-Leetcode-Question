ed1a40d1017e9e043c51518050cb31a946acc154
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-05 21:18:16 +0530

    Time: 3 ms (97.25%), Space: 9 MB (41.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,3 +1,36 @@
+//-----------------------BEST APPROACH OPTIMAL SOLUTION --------------------------
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freqTable[256] = {0};  // Initialize frequency table for all ASCII characters
+        
+        // Count frequencies of characters in the first string
+        for (int i = 0; i < s.size(); i++) {
+            freqTable[s[i]]++;
+        }
+        
+        // Decrease frequencies for characters in the second string
+        for (int i = 0; i < t.size(); i++) {
+            freqTable[t[i]]--;
+        }
+        
+        // Check if all frequencies are zero
+        for (int i = 0; i < 256; i++) {
+            if (freqTable[i] != 0) {
+                return false;  // If any character count is not zero, not an anagram
+            }
+        }
+        
+        return true;  // If all character counts are zero, it's an anagram
+    }
+};
+
+
+
+
+//------------------BRUTE FORCE USING THE SORTING ------------------------
+/*
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -14,3 +47,4 @@ public:
         }
     } 
 };
+*/
