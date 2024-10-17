eff5cf6c66e9b34f440c44d08d5d1a7e251bf885
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-18 04:00:48 +0530

    Time: 0 ms (100%), Space: 19.5 MB (86.61%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
+            
+        }
+        return ans;
+
+    }
+};
+
+
+
+
+//------------------------------using sorting --------------------------------------
+
+/*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i=i+2){
+            int ans = nums[i]^nums[i+1];
+            if(ans!=0){
+                return nums[i];
+            };
+        }
+        return nums[nums.size()-1];
+
+    }
+};
+*/
