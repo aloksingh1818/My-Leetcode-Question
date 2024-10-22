09582ea3f822949d51a2b77381b48b09b4513c05
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-22 11:58:14 +0530

    Time: 0 ms (100%), Space: 30.2 MB (21.73%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    int search(vector<int>& arr, int target) {
+        int l=0;
+        int h=arr.size()-1;
+        int mid=(l+h)/2;
+
+        while(l<=h){
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                l=mid+1;
+            }
+            if(arr[mid]>target){
+                h=mid-1;
+            }
+            mid=(l+h)/2;
+
+        }
+       return -1; 
+    }
+};
+
+
+
+//--------------------using linear search ------------------------
+
+/*
+
+
+
+class Solution {
+public:
     int search(vector<int>& nums, int target) {
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
@@ -9,3 +41,4 @@ public:
         return -1;
     }
 };
+*/
