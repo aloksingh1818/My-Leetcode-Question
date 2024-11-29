389d67f7fddedcc4831722af704b3765f685c605
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-11-29 21:05:10 +0530

    Time: 33 ms (70.08%), Space: 14.9 MB (78.8%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,24 @@
+// LeetCode Solution
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
